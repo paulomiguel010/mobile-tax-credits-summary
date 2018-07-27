1bad7e5558746344c621ee8e49574838041d67cf
Luke Smith <luke.smith@digital.hmrc.gov.uk>
2018-07-27 08:27:45 +0100

    [NGC-3329][LS] Changed to use wsUrl to it test correctly

--- it/uk/gov/hmrc/mobiletaxcreditssummary/api/PlatformIntegrationSpec.scala
+++ it/uk/gov/hmrc/mobiletaxcreditssummary/api/PlatformIntegrationSpec.scala
@@ -1,15 +1,12 @@
 package uk.gov.hmrc.mobiletaxcreditssummary.api
 
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
 import org.scalatest.concurrent.Eventually
 import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import org.scalatest.time.{Millis, Span}
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.{JsArray, JsValue}
-import play.api.test.Helpers._
-import play.api.test.{FakeRequest, PlayRunners}
-import uk.gov.hmrc.mobiletaxcreditssummary.controllers.api.DocumentationController
+import play.api.libs.ws.WSResponse
+import play.api.test.PlayRunners
 import uk.gov.hmrc.mobiletaxcreditssummary.stubs.ServiceLocatorStub._
 import uk.gov.hmrc.mobiletaxcreditssummary.support.BaseISpec
 
@@ -27,13 +24,6 @@ import uk.gov.hmrc.mobiletaxcreditssummary.support.BaseISpec
   * See: confluence ApiPlatform/API+Platform+Architecture+with+Flows
   */
 class PlatformIntegrationSpec extends BaseISpec with Eventually with PlayRunners {
-  trait Setup {
-    val documentationController: DocumentationController = app.injector.instanceOf[DocumentationController]
-    val request = FakeRequest()
-
-    implicit val system: ActorSystem = ActorSystem()
-    implicit val materializer: ActorMaterializer = ActorMaterializer()
-  }
 
   private val appId1: String = "00010002-0003-0004-0005-000600070008"
   private val appId2: String = "00090002-0003-0004-0005-000600070008"
@@ -51,37 +41,31 @@ class PlatformIntegrationSpec extends BaseISpec with Eventually with PlayRunners
     "register itself with the api platform automatically at start up" in {
       registerShouldNotHaveBeenCalled()
 
-      running(app) {
-        eventually(Timeout(Span(1000 * 20, Millis))) {
-          registerShouldHaveBeenCalled()
-        }
+      eventually(Timeout(Span(1000 * 20, Millis))) {
+        registerShouldHaveBeenCalled()
       }
     }
 
-    "provide definition with configurable whitelist" in new Setup {
-      running(app) {
-        val result = documentationController.definition()(request)
-        status(result) shouldBe 200
+    "provide definition with configurable whitelist" in {
+      val result: WSResponse = await(wsUrl("/api/definition").get())
+      result.status shouldBe 200
 
-        val definition: JsValue = contentAsJson(result)
-        val versions: Seq[JsValue] = (definition \ "api" \\ "versions").head.as[JsArray].value
-        versions.length shouldBe 1
+      val definition: JsValue = result.json
+      val versions: Seq[JsValue] = (definition \ "api" \\ "versions").head.as[JsArray].value
+      versions.length shouldBe 1
 
-        val versionJson: JsValue = versions.head
-        (versionJson \ "version").as[String] shouldBe "1.0"
+      val versionJson: JsValue = versions.head
+      (versionJson \ "version").as[String] shouldBe "1.0"
 
-        val accessDetails: JsValue = (versionJson \\ "access").head
-        (accessDetails \ "type").as[String] shouldBe "PRIVATE"
-        (accessDetails \ "whitelistedApplicationIds").head.as[String] shouldBe appId1
-        (accessDetails \ "whitelistedApplicationIds") (1).as[String] shouldBe appId2
-      }
+      val accessDetails: JsValue = (versionJson \\ "access").head
+      (accessDetails \ "type").as[String] shouldBe "PRIVATE"
+      (accessDetails \ "whitelistedApplicationIds").head.as[String] shouldBe appId1
+      (accessDetails \ "whitelistedApplicationIds") (1).as[String] shouldBe appId2
     }
 
-    "provide RAML conf endpoint" in new Setup {
-      running(app) {
-        val result = documentationController.conf("1.0", "application.raml")(request)
-        status(result) shouldBe 200
-      }
+    "provide RAML conf endpoint" in {
+      val result: WSResponse = await(wsUrl("/api/conf/1.0/application.raml").get())
+      result.status shouldBe 200
     }
   }
 }
